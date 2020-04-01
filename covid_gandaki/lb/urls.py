0c9062a1b773a343c241b8e816f4bb483e3d3c1a
Anup Adhikari <alwaysgetwithanup@gmail.com>
2020-04-02 03:58:52 +0545

    some changes to index files

--- covid_gandaki/lb/urls.py
+++ covid_gandaki/lb/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from django.conf.urls import include, url
 from covid_gandaki.lb import views
+from covid_gandaki.public import views as pv
 
 urlpatterns = [
     path('', views.index, name='dashboard'),
     path('submission', views.submit, name='submit'),
     path('dtable', views.index_dtable, name='tabulator'),
+    path('food_info', pv.index, name='khadya' ),
 ]
 
 app_name = "lb"
