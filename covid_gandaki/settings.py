9889cfe43cb63cf64868e1db1862baeac4d6bafe
Anup Adhikari <alwaysgetwithanup@gmail.com>
2020-04-01 14:34:06 +0545

    settings fix

--- covid_gandaki/settings.py
+++ covid_gandaki/settings.py
@@ -156,11 +156,3 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ]
 }
-
-AUTHENTICATION_BACKENDS = (
-    # Needed to login by custom User model, regardless of `allauth`
-    "django.contrib.auth.backends.ModelBackend",
-
-    # `allauth` specific authentication methods, such as login by e-mail
-    "allauth.account.auth_backends.AuthenticationBackend",
-)
