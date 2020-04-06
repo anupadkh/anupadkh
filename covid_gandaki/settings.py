3c0d9cdbd6418bc3863d7379776226b9ef7a09e4
Anup Adhikari <alwaysgetwithanup@gmail.com>
2020-04-06 15:05:41 +0545

    authenticated_users_access_rest

--- covid_gandaki/settings.py
+++ covid_gandaki/settings.py
@@ -161,7 +161,7 @@ REST_FRAMEWORK = {
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
         # 'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnlyIsAuthenticated'
+        'rest_framework.permissions.IsAuthenticated'
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication',
