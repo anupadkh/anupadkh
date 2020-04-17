fd62b247132259db09f7d701d7db5ef1d14317c5
Anup Adhikari <alwaysgetwithanup@gmail.com>
2020-04-17 11:29:46 +0545

    migrating to postgres

--- covid_gandaki/settings.py
+++ covid_gandaki/settings.py
@@ -98,18 +98,18 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # },
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME' : 'covid',
-        'USER'  : 'postgres',
-        'PASSWORD' : 'Adhik@ri1',
-        'HOST' : 'localhost',
-        'PORT': '5432'
-    }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME' : 'covid',
+    #     'USER'  : 'postgres',
+    #     'PASSWORD' : 'Adhik@ri1',
+    #     'HOST' : 'localhost',
+    #     'PORT': '5432'
+    # }
 
 
 }
