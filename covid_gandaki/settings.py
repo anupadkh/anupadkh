e360fcbf5311907b2091d8db2a4f7cfad4877c7a
Anup Adhikari <alwaysgetwithanup@gmail.com>
2020-04-17 10:39:29 +0545

    postgres USER

--- covid_gandaki/settings.py
+++ covid_gandaki/settings.py
@@ -105,7 +105,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME' : 'moitfe',
-        'USER'  : 'moitfe',
+        'USER'  : 'postgres',
         'PASSWORD' : 'Adhik@ri1',
         'HOST' : '127.0.0.1',
         'PORT': '5432'
