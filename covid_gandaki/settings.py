c59b224f0090b9343789041ef7ca1fd33c43fb99
Anup Adhikari <alwaysgetwithanup@gmail.com>
2020-04-17 09:14:37 +0545

    postgres exports

--- covid_gandaki/settings.py
+++ covid_gandaki/settings.py
@@ -98,10 +98,20 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # },
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME' : 'postgres',
+        'USER'  : 'moitfe',
+        'PASSWORD' : 'Adhik@ri1',
+        'HOST' : '127.0.0.1',
+        'PORT': '5432'
     }
+
+
 }
 
 
